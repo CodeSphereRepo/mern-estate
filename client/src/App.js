2a4807ad3c1bdaa605c97b3f64d199c262f1c0bd
CodeSphereRepo <no.hannandar21@yahoo.com>
2024-05-12 01:52:34 +0500

    First

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <h1 className="text-3xl font-bold underline text-pink-600 mt-6">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
