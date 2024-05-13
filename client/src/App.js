81d59ccf236716ed080853245cdcffdb6a2061ea
CodeSphereRepo <no.hannandar21@yahoo.com>
2024-05-14 03:15:41 +0500

    create pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signup from './pages/Signup.js';
+import Home from './pages/Home.js';
+import Signin from './pages/Signin.js';
+import About from './pages/About.js';
+import Profile from './pages/Profile.js';
+import Search from './pages/Search.js';
 
 function App() {
   return (
-    <div>
-     <h1 className="text-3xl font-bold underline text-pink-600 mt-6">
-      Hello world!
-    </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/search' element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
