38f244f9b64fcb2ee0ac393da1cec9542750d717
CodeSphereRepo <no.hannandar21@yahoo.com>
2024-05-17 02:37:40 +0500

    Create signup Api

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -1,6 +1,21 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
 export const signUp = async (req, res) => {
-  console.log(req.body);
-  // You can add more functionality here for the sign up process
+  const { username, password, email } = req.body;
+
+  try {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user with the hashed password
+    const newUser = new User({ username, password: hashedPassword, email });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
