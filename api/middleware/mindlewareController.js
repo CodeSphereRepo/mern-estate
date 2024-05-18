fc6ed9a607523d58d3853156c0a5ad87a62776a8
CodeSphereRepo <no.hannandar21@yahoo.com>
2024-05-18 07:15:18 +0500

    create a middleware code

---
+++ api/middleware/mindlewareController.js
@@ -0,0 +1,19 @@
+export const validateSignUp = (req, res, next) => {
+    const { username, email, password } = req.body;
+  
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: 'Username, email, and password are required' });
+    }
+  
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: 'Invalid email format' });
+    }
+  
+    if (password.length < 6) {
+      return res.status(400).json({ message: 'Password must be at least 6 characters long' });
+    }
+  
+    next();
+  };
+  
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { signUp } from "../controller/userController.js"
+import { validateSignUp } from '../middleware/mindlewareController.js';
 
 const router = express.Router();
 
 // Route to sign up a new user
-router.post('/signup', signUp);
+router.post('/signup',validateSignUp, signUp);
 
 // Additional routes for user-related operations can be added here
 
